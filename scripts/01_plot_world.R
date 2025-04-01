896f68134a36dfb88756da2d26905f94c6931253
SnBuenafe <tinbuenafe@gmail.com>
2025-04-02 09:57:10 +1000

    Plotting script

---
+++ scripts/01_plot_world.R
@@ -0,0 +1,26 @@
+# Let's plot some cool stuff
+
+# Install packages: uncomment if needed
+# install.packages(c("rnaturalearth", "tidyverse", "sf"))
+
+# Load packages
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+
+# Define objects
+world <- ne_countries(type = "countries",
+                      scale = "large")
+world
+australia <- ne_states(country = "Australia")
+australia
+
+# Plot the world
+ggplot() +
+  geom_sf(data = world, fill = "grey64", color = "black") +
+  theme_bw()
+
+# Plot the country
+ggplot() +
+  geom_sf(data = australia, fill = "grey64", color = "black") +
+  theme_bw()
