e53aa92fb35b10b7cc68acb698db406923d3efcb
lkbentley <lily.bentley@gmail.com>
2025-04-02 14:15:35 +1000

    update maps

--- scripts/01_plot_world.R
+++ scripts/01_plot_world.R
@@ -24,3 +24,21 @@ ggplot() +
 ggplot() +
   geom_sf(data = australia, fill = "grey64", color = "black") +
   theme_bw()
+
+# Add Australian capital cities
+# Create a data frame with capital cities coordinates
+aus_capitals <- data.frame(
+  city = c("Canberra", "Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide", "Hobart", "Darwin"),
+  lon = c(149.1300, 151.2093, 144.9631, 153.0251, 115.8575, 138.6007, 147.3272, 130.8456),
+  lat = c(-35.2809, -33.8688, -37.8136, -27.4698, -31.9505, -34.9285, -42.8821, -12.4634)
+)
+
+# Plot Australia with capital cities as stars
+ggplot() +
+  geom_sf(data = australia, fill = "grey64", color = "black") +
+  geom_point(data = aus_capitals, aes(x = lon, y = lat),
+             shape = 8, size = 3, color = "red") +  # Shape 8 is a star
+  geom_text(data = aus_capitals, aes(x = lon, y = lat, label = city),
+            nudge_y = -0.5, size = 3) +
+  labs(title = "Australian States with Capital Cities") +
+  theme_bw()
